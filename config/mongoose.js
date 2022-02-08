b3f4e09809ef674d14bac6366a59ea77a1a06e40
Smanika <67970197+Smanika@users.noreply.github.com>
2022-02-08 10:03:33 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/movieDB');
+mongoose.connect('url database url');
 
 const db= mongoose.connection
 db.on('error',console.error.bind(console,'error in database')) // errors
